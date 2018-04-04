e55f5effb59d365bc430ca8b504fbbe9025b5921
Tirta Nugraha <dev.nugrata@gmail.com>
2018-04-04 11:13:04 +0700

    other(style): use border instead of background

--- shared/container/DemoApp/Header/Menu/index.js
+++ shared/container/DemoApp/Header/Menu/index.js
@@ -6,7 +6,11 @@ import Link from 'react-router-dom/Link';
 function Menu() {
   return (
     <ul
-      style={{ marginTop: '1rem', padding: '1rem', backgroundColor: '#c6c6c6' }}
+      style={{
+        marginTop: '1rem',
+        padding: '1rem',
+        border: '1px solid #c6c6c6',
+      }}
     >
       <li>
         <Link to="/">Home</Link>
