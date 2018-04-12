a48240510334b350c0f4123687be2bf48519f656
Tirta Nugraha <dev.nugrata@gmail.com>
2018-04-12 19:30:02 +0700

    other(loading): adding loading component

--- shared/container/DemoApp/AsyncPostRoute/index.js
+++ shared/container/DemoApp/AsyncPostRoute/index.js
@@ -1,8 +1,16 @@
+import React, { Component } from 'react';
 import { asyncComponent } from 'react-async-component';
 
 export default asyncComponent({
   // include home and about route in same chunk e.g post
   resolve: () => import(/* webpackChunkName: "post" */ './PostRoute'),
+  LoadingComponent: ({ props }) => <LoadingCaption />,
   ssrMode: 'boundary',
   name: 'Post',
 });
+
+class LoadingCaption extends Component {
+  render() {
+    return <div>loading...</div>;
+  }
+}
