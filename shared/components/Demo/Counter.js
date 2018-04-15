772bce530ac4e8b29bcde5a45d764f01af7efbfe
Tirta Nugraha <dev.nugrata@gmail.com>
2018-04-15 15:54:37 +0700

    fix(eslint): fix eslint warning

--- shared/components/Demo/Counter.js
+++ shared/components/Demo/Counter.js
@@ -6,7 +6,7 @@ export default class Counter extends Component {
     super(props);
     this.state = {};
   }
-  static getDerivedStateFromProps(nextProps) {
+  static getDerivedStateFromProps() {
     return null;
   }
   render() {
