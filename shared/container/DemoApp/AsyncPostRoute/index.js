f83a6bd292a80009baf71f947223e418c0af753c
Tirta Nugraha <dev.nugrata@gmail.com>
2018-09-29 22:14:37 +0700

    build(lint): remove loading component for lint fix

--- shared/container/DemoApp/AsyncPostRoute/index.js
+++ shared/container/DemoApp/AsyncPostRoute/index.js
@@ -4,13 +4,6 @@ import { asyncComponent } from 'react-async-component';
 export default asyncComponent({
   // include home and about route in same chunk e.g post
   resolve: () => import(/* webpackChunkName: "PostRoute" */ './PostRoute'),
-  LoadingComponent: ({ props }) => <LoadingCaption />,
   ssrMode: 'boundary',
   name: 'Post',
 });
-
-class LoadingCaption extends Component {
-  render() {
-    return <div>loading...</div>;
-  }
-}
