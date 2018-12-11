cc945cfbbf864a60d947a158d8b5af3f5c90020b
Tirta Nugraha <dev.nugrata@gmail.com>
2018-12-11 14:41:57 +0700

    refactor(eslint): fix eslint warning

--- shared/container/DemoApp/AsyncPostRoute/__tests__/PostRoute.test.js
+++ shared/container/DemoApp/AsyncPostRoute/__tests__/PostRoute.test.js
@@ -2,14 +2,10 @@
 
 import React from 'react';
 import { shallow } from 'enzyme';
-import configureStore from 'redux-mock-store';
 import PostRoute from '../PostRoute';
-import { postMock } from './../../../../redux/initialStates';
 
 describe('<PostRoute />', () => {
   test('renders', () => {
-    const mockStore = configureStore();
-    const store = mockStore(postMock);
     const wrapper = shallow(<PostRoute />);
     expect(wrapper).toMatchSnapshot();
   });
