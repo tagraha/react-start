bb3b78078017a047751248148c698b58603312ed
Tirta Nugraha <dev.nugrata@gmail.com>
2018-12-18 16:51:28 +0700

    test(postRoute): update snapshot testing

--- shared/container/DemoApp/AsyncPostRoute/__tests__/PostRoute.test.js
+++ shared/container/DemoApp/AsyncPostRoute/__tests__/PostRoute.test.js
@@ -1,12 +1,14 @@
-/* eslint-disable import/no-extraneous-dependencies */
-
 import React from 'react';
 import { shallow } from 'enzyme';
+import configureStore from 'redux-mock-store';
 import PostRoute from '../PostRoute';
+import { postMock } from './../../../../redux/initialStates';
 
 describe('<PostRoute />', () => {
   test('renders', () => {
-    const wrapper = shallow(<PostRoute />);
+    const mockStore = configureStore();
+    const store = mockStore(postMock);
+    const wrapper = shallow(<PostRoute store={store}/>);
     expect(wrapper).toMatchSnapshot();
   });
 });
