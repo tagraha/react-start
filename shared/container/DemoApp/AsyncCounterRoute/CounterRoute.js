6869193e3d07cabba2e542d963a5dc0f12d2ae4a
Tirta Nugraha <dev.nugrata@gmail.com>
2020-04-07 12:33:48 +0700

    fix(hook): fix invalid hook call on server side

--- shared/container/DemoApp/AsyncCounterRoute/CounterRoute.js
+++ shared/container/DemoApp/AsyncCounterRoute/CounterRoute.js
@@ -1,32 +1,41 @@
 /* eslint-disable react/no-unescaped-entities */
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 
-const CounterRoute = () => {
-  const [counter, setCounter] = useState(0);
+class CounterRoute extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0,
+    };
+  }
 
-  const incrementCounter = () => {
-    setCounter(counter + 1);
+  incrementCounter = () => {
+    const { counter } = this.state;
+    this.setState({ counter: counter + 1 });
   };
 
-  return (
-    <div>
-      <h3>
-        Counter <small>non redux</small>
-      </h3>
-      <p>
-        <em>
-          This is a small demo component that contains state. It's useful for
-          testing the hot reloading experience of an asyncComponent.
-        </em>
-      </p>
-      <p>Current value: {counter}</p>
-      <p>
-        <button type="button" onClick={incrementCounter}>
-          Increment
-        </button>
-      </p>
-    </div>
-  );
-};
+  render() {
+    const { counter } = this.state;
+    return (
+      <div>
+        <h3>
+          Counter <small>non redux</small>
+        </h3>
+        <p>
+          <em>
+            This is a small demo component that contains state. It's useful for
+            testing the hot reloading experience of an asyncComponent.
+          </em>
+        </p>
+        <p>Current value: {counter}</p>
+        <p>
+          <button type="button" onClick={this.incrementCounter}>
+            Increment
+          </button>
+        </p>
+      </div>
+    );
+  }
+}
 
 export default CounterRoute;
